cd2c61a7b7c0776ea8d2c22a25c1052f1073b6b8
Samuel Muigai <ndungusam01@gmail.com>
2024-01-27 10:37:53 +0300

    Views done but tests still failing

--- server/app.py
+++ server/app.py
@@ -25,16 +25,43 @@ def animal_by_id(id):
         <li>ID: {animal.id}</li>
         <li>Name: {animal.name}</li>
         <li>Species: {animal.species}</li>
-        <li.Zookeeper: {animal.zookeeper}</li>
+        <li>Zookeeper: {animal.zookeeper.name}</li>
+        <li>Enclosure: {animal.enclosure.environment}</li>
+    </ul>
     '''
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper=Zookeeper.query.filter(Zookeeper.id == id).first()
+
+    animal_body=""
+    for animal in zookeeper.animals:
+        animal_body+=f"<li>Animal: {animal.name}</li>"
+
+    return f'''
+    <ul>
+        <li>ID: {zookeeper.id}</li>
+        <li>Name: {zookeeper.name}</li>
+        <li>Birthday: {zookeeper.birthday}</li>
+        {animal_body}
+    </ul>
+    '''
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure=Enclosure.query.filter(Enclosure.id == id).first()
+
+    animal_body=""
+    for animal in enclosure.animals:
+        animal_body+=f"<li>Animal: {animal.name}</li>"
+    return f'''
+    <ul>
+        <li>ID: {enclosure.id}</li>
+        <li>Environment: {enclosure.environment}</li>
+        <li>Open to Visitors: {enclosure.open_to_visitors}</li>
+        {animal_body}
+    </ul>
+    '''
 
 
 if __name__ == '__main__':
